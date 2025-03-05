1fc934c90f295ebb23f0f91b2ac9fc906dbdee63
rohitsriv28 <rohitraj2002ind@gmail.com>
2025-03-05 19:43:20 +0530

    4th: Navbar and Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="mx-4 sm:mx-[-10]">
+    <div className="mx-4 sm:mx-[10%]">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
