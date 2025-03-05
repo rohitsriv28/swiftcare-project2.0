574630b85ebdff407d977326da1e5286d5dfb2fe
rohitsriv28 <rohitraj2002ind@gmail.com>
2025-03-05 21:53:58 +0530

    5th: Half HomePage done.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import SpecialityMenu from "../components/SpecialityMenu";
+import TopDocs from "../components/TopDocs";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <SpecialityMenu />
+      <TopDocs />
     </div>
   );
 };
