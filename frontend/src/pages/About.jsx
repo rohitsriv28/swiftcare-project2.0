fcbd5b96ef2073a29fdd75e2edf1b1f5963b2645
rohitsriv28 <rohitraj2002ind@gmail.com>
2025-03-11 19:19:48 +0530

    8th: About and Contact pages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>
+          ABOUT <span className="text-gray-700 font-medium">US</span>
+        </p>
+        <p className="text-md pt-5">
+          Our mission is to provide affordable and accessible healthcare to
+          everyone. We connect patients with doctors in a secure and safe
+          environment. We are committed to providing the best healthcare
+          services to our patients.
+        </p>
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-12">
+        <img src={assets.about_image} alt="" className="w-full max-w-[360px]" />
+        <div className="flex flex-col justify-center gap-6 w-2/4 text-sm text-gray-600">
+          <p>
+            Welcome to SwiftCare, your trusted partner in managing your
+            healthcare needs conveniently and efficiently. At SwiftCare, we
+            understand the challenges individuals face when it comes to
+            scheduling doctor appointments and managing their health records.
+          </p>
+          <p>
+            SwiftCare is committed to excellence in healthcare technology. We
+            continuously strive to enhance our platform, integrating the latest
+            advancements to improve user experience and deliver superior
+            service. Whether you're booking your first appointment or managing
+            ongoing care, SwiftCare is here to support you every step of the
+            way.
+          </p>
+          <b className="text-gray-800">Our Vision</b>
+          <p>
+            Our vision at SwiftCare is to create a seamless healthcare
+            experience for every user. We aim to bridge the gap between patients
+            and healthcare providers, making it easier for you to access the
+            care you need, when you need it.
+          </p>
+        </div>
+      </div>
+      <div>
+        <p className="text-xl my-4">
+          WHY <span className="text-gray-700 font-semibold">CHOOSE US</span>
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Efficiency:</b>
+          <p>
+            Stramlined appointment scheduling that fits into your busy
+            lifestyle.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-500 text-gray-600 cursor-pointer">
+          <b>Convinience:</b>
+          <p>
+            Access to a network of trusted healthcare professionals in your
+            area.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Personilization:</b>
+          <p>
+            Tailored recommendations and reminders to help you stay on top of
+            your health.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
